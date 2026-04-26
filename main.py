c4a300603d4d3f9489a4aa8de5456708b7537864
Sai Dinesh Dacharaju <saidinesh95@gmail.com>
2026-04-26 14:05:51 +0200

    changed farewell greeting for practice

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def greet(name):
     return f"Hello, {name}!"
 
 def farewell(name):
-    return f"See you later, {name}!"
+    return f"Hasta la vista, {name}!"
 
 print(greet("Dinesh"))
 print(farewell("Dinesh"))
